6e9d20abd33dd4fad2776b05e7e424852a9f1cac
Serubin <Serubin@Serubin.net>
2020-02-06 23:04:20 -0800

    Add global worker

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import SimpleWebWorker from 'simple-web-worker'
 export default {
   install: function(Vue, name) {
     name = name || '$worker'
+    Vue.worker = SimpleWebWorker
     Object.defineProperty(Vue.prototype, name, { value: SimpleWebWorker })
   }
 }
